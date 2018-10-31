a7826bc7b2c6d908a2cb5dc6a15f7ad32d9499f7
sudarminkuh <43722667+sudarminkuh@users.noreply.github.com>
2018-10-31 07:31:06 -0700

    Add files via upload

---
+++ ArrayListDouble.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class ArrayListDouble {
+
+public static void main(String[] args) {
+ArrayList<Double> wakanda = new ArrayList<Double>();
+		
+		wakanda.add(4.7);
+		wakanda.add(4.2);
+		wakanda.add(9.4);
+		wakanda.add(5.6);
+		wakanda.add(5.9);
+		
+		
+		System.out.println("tadaaaaa ini hasilnya : " +wakanda);
+
+	}
+
+}
